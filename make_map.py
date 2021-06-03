10569b1cbb62dfdc122e2059f4d27efca5db5f30
peter <petrel.harp@gmail.com>
2021-06-02 23:21:17 -0700

    .

--- make_map.py
+++ make_map.py
@@ -3,9 +3,9 @@ import numpy as np
 import PIL.Image as Image
 
 
-H, W = (50, 100)
+H, W = (10, 15)
 res = 0.1
-p = 0.01
+p = 0.005
 h, w = (int(H / res), int(W / res))
 habitat = np.zeros(h * w, dtype=np.uint8)
 habitat[np.random.uniform(size=h * w) < p] = 255
