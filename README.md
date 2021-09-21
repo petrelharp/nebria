3327c36e1271cc621ead36ad3c6d31e480ddf0b3
peter <petrel.harp@gmail.com>
2021-09-21 06:29:06 -0700

    neg binom dispersal

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 - run `Rscript make_maps.R` to make the maps that SLiM uses
 
 - `.tif` files: Yi-Ming says (3 June 2021) " The file names contain the name of the time period and the chunk of time. For example, BA13_15 means BA period that is from 15 kyr to 13 kyr."
+
+- To run a simulation copy the required map files into a subdirectory,
+    make a `params.json` JSON file with parameters that change the default,
+    and run `slim ../nebria.slim` from within that directory.
--- plot_dispersal.R
+++ plot_dispersal.R
@@ -6,10 +6,15 @@ all_habitat <- t(habitat[rev(seq_len(nrow(habitat))),,1])
 habitat <- t(habitat[rev(seq_len(nrow(habitat))),,2])
 
 defineConstant <- assign
-defineConstant("P_D", 0.2); # // dispersal probability
+defineConstant("P_D", 0.1); # // dispersal probability
 defineConstant("MEAN_HOPS", 20); # // mean number of dispersal hops
-defineConstant("SIGMA", 0.01); # // dispersal distance
-defineConstant("MAP_RES", 0.01); # // resolution
+defineConstant("DISPERSAL_SIGMA", 1.0); # // dispersal distance
+defineConstant("MAP_RES_X", 0.742); # // resolution in km (see make_maps.R)
+defineConstant("MAP_RES_Y", 0.925); # // resolution in km (see make_maps.R)
+defineConstant("STEPSIZE", 1); #   // years per generation (to make debugging go faster)
+defineConstant("SIGMA", DISPERSAL_SIGMA * sqrt(STEPSIZE));
+
+stopifnot(STEPSIZE == 1)
 
 loc_to_map <- function (loc) {
     loc <- rbind(loc)
@@ -17,8 +22,8 @@ loc_to_map <- function (loc) {
     x1 <- extent(suitability)[2]
     y0 <- extent(suitability)[3]
     y1 <- extent(suitability)[4]
-    x <- x0 + (x1 - x0) * loc[,1] / MAP_RES / ncol(habitat)
-    y <- y0 + (y1 - y0) * loc[,2] / MAP_RES / nrow(habitat)
+    x <- x0 + (x1 - x0) * loc[,1] / MAP_RES_X / nrow(habitat)
+    y <- y0 + (y1 - y0) * loc[,2] / MAP_RES_Y / ncol(habitat)
     return(cbind(x,y))
 }
 
@@ -45,11 +50,14 @@ p1.spatialMapValue <- function(map, loc) {
 
 reproduction <- function (loc, P_D=1) {
     path <- rbind(c(loc, NA))
-			num_hops = 0;
+            loc = loc + rnorm(2, mean=0, sd=SIGMA / sqrt(MEAN_HOPS));
+    path <- rbind(c(loc, 1))
+			num_hops = 1;
 			tries = 0;
 			if (runif(1) < P_D) {
-				k = rgeom(1, 1/MEAN_HOPS);
-				while (num_hops < k & tries < 1000) {
+                # // NegBinom(r, p) is Pois(Gamma(shape=r, rate=(1-p)/p))
+                k = 1 + rpois(1, rexp(1, 1/MEAN_HOPS));
+				while (num_hops < k & tries < 100) {
                     current = p1.spatialMapValue('suitability', loc);
 					next_loc = loc + rnorm(2, mean=0, sd=SIGMA / sqrt(MEAN_HOPS));
 					if (runif(1) * current <= p1.spatialMapValue('suitability', next_loc)) {
@@ -87,8 +95,8 @@ plot_path <- function (path) {
     )
 }
 
-good_locs <- cbind(row(habitat)[habitat > 0.1],
-                col(habitat)[habitat > 0.1]) * MAP_RES
+good_locs <- cbind(row(habitat)[habitat > 0.1] * MAP_RES_X,
+                   col(habitat)[habitat > 0.1] * MAP_RES_Y)
 stopifnot(all(p1.pointInBounds(good_locs)))
 
 good_points <- loc_to_map(good_locs)
@@ -97,8 +105,30 @@ paths <- lapply(1:nrow(good_locs), function (k)
                 reproduction(good_locs[k,])
             )
 
-pdf(file="example_dispersal.pdf", width=8, height=8, pointsize=10)
-    plot(suitability)
+# note NegBinom(r, p) is Pois(Gamma(shape=r, rate=(1-p)/p))
+rdispersal_dist <- function (n, shape=100) {
+    nhops <- 1 + rpois(n, rgamma(n, shape=shape, rate=shape/MEAN_HOPS))
+    xy <- cbind(
+            rnorm(n, sd=sqrt(nhops) * SIGMA / sqrt(MEAN_HOPS)),
+            rnorm(n, sd=sqrt(nhops) * SIGMA / sqrt(MEAN_HOPS))
+    )
+    return(sqrt(rowSums(xy^2)))
+}
+
+pdf(file="example_dispersal.pdf", width=10, height=5, pointsize=10)
+layout(t(1:3))
+    x <- rdispersal_dist(1e4, shape=1)
+    hist(x, breaks=100,
+         main="dispersal distance (km)")
+    xx <- seq(0.1, 10, length.out=101)
+    plot(xx, type='l',
+         sapply(xx, function (z) mean(x * z / SIGMA > 5)),
+         xlab='SIGMA',
+         ylab='proportion > 5',
+         log='y'
+    )
+    abline(v=1, lty=3)
+    plot(suitability, main=sprintf("SIGMA=%0.1f", SIGMA))
     points(good_points, cex=5)
     for (path in paths) {
         plot_path(path)
