5d2b682ff5a2fa6dbe409235c100418980233bcd
peter <petrel.harp@gmail.com>
2022-03-13 22:13:28 -0700

    plot script

--- param_grid/plot_genetic_stats.R
+++ param_grid/plot_genetic_stats.R
@@ -4,5 +4,36 @@ basedir = "post_21000/"
 all_stats <- read.csv(paste0(basedir, "stats_all.csv"))
 all_pairstats <- read.csv(paste0(basedir, "pairstats_all.csv"))
 
-ggplot(all_stats, aes(x = rep, color = rep, y = het)) +
-  geom_point()
+all_stats$sim <- factor(sapply(strsplit(all_stats$rep, "_"), "[", 3))
+all_stats$recap <- factor(sapply(strsplit(all_stats$rep, "_"), "[", 5))
+all_stats$mut <- factor(sapply(strsplit(all_stats$rep, "_"), "[", 6))
+all_stats$sample <- sapply(strsplit(all_stats$rep, "_"), "[", 7)
+
+# Look at variation within a few simulations
+pdf(file="within-sim-variation.pdf", width=8, height=10, pointsize=10)
+layout(matrix(1:6, ncol=2))
+site_list <- c("Conness Lake", "Sixty Lakes", "Army Pass")
+for (sim_id in c("2633463977134", "1843169324096", "1745513977159", "3424803977142", "2633493977134", "2607102226138")) {
+    plot(0, type='n', xlim=c(0, 125), ylim=range(0, all_stats$het), xlab='Index', ylab='heterozygosity',
+         main=paste("sim ", sim_id)
+    )
+    for (k in seq_along(site_list)) {
+        site <- site_list[k]
+        with(droplevels(subset(all_stats, sim == sim_id & site_name == site)), {
+             points(het,
+                    pch=20,
+                    col=k,
+            )
+            abline(v=0.5 + which(diff(as.numeric(recap)) > 0), lty=1)
+            abline(v=0.5 + which(diff(as.numeric(mut)) > 0), lty=3)
+        })
+    }
+    legend("bottomright",
+           pch=c(NA, NA, rep(20, length(site_list))),
+           lty=c(1, 3, rep(NA, length(site_list))),
+           col=c(1, 1, seq_along(site_list)),
+           legend=c("recap reps", "mutation reps", site_list),
+           bg='white'
+    )
+}
+dev.off()
