13c35f808fac93bdb032c4ed854ba51aa0c20370
Gilia Patterson <32884936+giliapatterson@users.noreply.github.com>
2023-01-31 12:17:01 -0800

    Combine results from simulations with mutation rate changes

--- param_grid/combine_and_clean_genetic_stats.R
+++ param_grid/combine_and_clean_genetic_stats.R
@@ -5,13 +5,13 @@ source("../data/helpers.R")
 long_break <- -118.68
 
 # the data
-observed_stats <- read.csv(file.path("../observed", "observe.stats.csv"))
+observed_stats <- read_csv(file.path("../observed", "observe.stats.csv"))
 observed_stats$site_name <- transl(observed_stats$site_name)
-observed_pairstats <- read.csv(file.path("../observed", "observe.pairstats.csv"))
+observed_pairstats <- read_csv(file.path("../observed", "observe.pairstats.csv"))
 observed_pairstats$loc1 <- transl(observed_pairstats$loc1)
 observed_pairstats$loc2 <- transl(observed_pairstats$loc2)
 observed_pairstats$pair_name <- make_names(short_name(observed_pairstats$loc1), short_name(observed_pairstats$loc2))
-sample_locs <- read.csv("../sample_locs.csv")
+sample_locs <- read_csv("../sample_locs.csv")
 sample_locs$site_name <- transl(sample_locs$site_name)
 stopifnot(nrow(observed_stats) == nrow(sample_locs))
 observed_stats <- merge(observed_stats, sample_locs)
@@ -27,19 +27,22 @@ observed_pairstats$group <- paste(
 )
 
 # Combine two simulation folders
-basedir1 = "post_21000"
-basedir2 = "post_21000_2022-12-05"
+#basedir1 = "post_21000"
+#basedir2 = "post_21000_2022-12-05"
 
-all_stats1 <- read.csv(file.path(basedir1, "stats_all.csv"))
-all_stats2 <- read.csv(file.path(basedir2, "stats_all.csv"))
+#all_stats1 <- read.csv(file.path(basedir1, "stats_all.csv"))
+#all_stats2 <- read.csv(file.path(basedir2, "stats_all.csv"))
 
-all_pairstats1 <- read.csv(file.path(basedir1, "pairstats_all.csv"))
-all_pairstats2 <- read.csv(file.path(basedir2, "pairstats_all.csv"))
+#all_pairstats1 <- read.csv(file.path(basedir1, "pairstats_all.csv"))
+#all_pairstats2 <- read.csv(file.path(basedir2, "pairstats_all.csv"))
 
 # Combine
-all_stats <- bind_rows(all_stats1, all_stats2)
-all_pairstats <- bind_rows(all_pairstats1, all_pairstats2)
+#all_stats <- bind_rows(all_stats1, all_stats2)
+#all_pairstats <- bind_rows(all_pairstats1, all_pairstats2)
 
+basedir = "post_21000_2022-12-05"
+all_stats <- read_csv(file.path(basedir, "stats_all.csv"))
+all_pairstats <- read_csv(file.path(basedir, "pairstats_all.csv"))
 all_stats$sim <- factor(sapply(str_match_all(all_stats$rep, "sim_([:digit:]+)_stats/stats_([:digit:]+)_([:digit:]+)_([:digit:]+)"), "[", 2))
 all_stats$recap <- factor(sapply(str_match_all(all_stats$rep, "sim_([:digit:]+)_stats/stats_([:digit:]+)_([:digit:]+)_([:digit:]+)"), "[", 3))
 all_stats$mut <- factor(sapply(str_match_all(all_stats$rep, "sim_([:digit:]+)_stats/stats_([:digit:]+)_([:digit:]+)_([:digit:]+)"), "[", 4))
@@ -53,7 +56,7 @@ rep_info$recap <- factor(sapply(str_match_all(rep_info$rep, "sim_([:digit:]+)_st
 rep_info$mut <- factor(sapply(str_match_all(rep_info$rep, "sim_([:digit:]+)_stats/stats_([:digit:]+)_([:digit:]+)_([:digit:]+)"), "[", 4))
 rep_info$sample <- factor(sapply(str_match_all(rep_info$rep, "sim_([:digit:]+)_stats/stats_([:digit:]+)_([:digit:]+)_([:digit:]+)"), "[", 5))
 
-for (pn in c("T2", "T1", "CS", "AS", "NE", "Na", "Nc", "Ns", "POP_SIZE", "P_D", "YEAR_SHAPE")) {
+for (pn in c("T2", "T1", "CS", "AS", "NE", "Na", "Nc", "Ns", "mut_rate", "POP_SIZE", "P_D", "YEAR_SHAPE")) {
   rep_info[[pn]] <- NA
   for (j in 1:nrow(rep_info)) {
     this_rep <- rep_info$rep[j]
@@ -98,21 +101,23 @@ ut <- (observed_pairstats$loc1 != observed_pairstats$loc2)
 observed[sprintf("dxy_%s", observed_pairstats$pair_name[ut])] <- observed_pairstats$dxy[ut]
 
 # Write to file
+results_folder <- file.path(basedir, "cleaned_results")
+dir.create(results_folder)
 # Observed heterozygosity and pairwise stats
-write.table(observed, "cleaned_results/observed.csv", sep = ",")
+write.table(observed,  file.path(results_folder, "observed.csv"), sep = ",")
 # Simulated heterozygosity and pairwise stats
-write.csv(stats, "cleaned_results/stats.csv")
+write.csv(stats, file.path(results_folder, "stats.csv"))
 # Parameters for each simulation and replicate
-write.csv(rep_info, "cleaned_results/rep_info.csv")
+write.csv(rep_info, file.path(results_folder, "rep_info.csv"))
 # Sampling locations
-write.csv(sample_locs, "cleaned_results/sample_locs.csv")
+write.csv(sample_locs, file.path(results_folder, "sample_locs.csv"))
 # Stats for all simulations
-write.csv(all_stats, "cleaned_results/all_stats.csv")
+write.csv(all_stats, file.path(results_folder, "all_stats.csv"))
 # Pairwise stats for all simulations
-write.csv(all_pairstats, "cleaned_results/all_pairstats.csv")
+write.csv(all_pairstats, file.path(results_folder, "all_pairstats.csv"))
 
-write.csv(observed_stats, "cleaned_results/observed_stats.csv")
-write.csv(observed_pairstats, "cleaned_results/observed_pairstats.csv")
+write.csv(observed_stats, file.path(results_folder, "observed_stats.csv"))
+write.csv(observed_pairstats, file.path(results_folder, "observed_pairstats.csv"))
 
 
 
